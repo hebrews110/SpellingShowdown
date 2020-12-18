b0cce5eecb143ab77e19bea784e9afa20350cfba
Themba Dube <embeddedthemba@gmail.com>
2020-12-18 14:12:57 -0500

    Add lesson 133 list

---
+++ lists/la7/lesson133.js
@@ -0,0 +1,40 @@
+var list = [
+    'acres',
+    'adjustable',
+    'afghan',
+    'analysis',
+    'artificial',
+    'absolutely',
+    'achievement',
+    'bouquet',
+    'congratulations',
+    'confetti',
+    'capital',
+    'biscuit',
+    'bureau',
+    'culprit',
+    'competition',
+    'embarrass',
+    'description',
+    'engineer'
+];
+
+var sentences = [
+    "I bought several acres of land.",
+    "The height of this desk is adjustable.",
+    "I have completed my analysis of the situation.",
+    "There are too many artificial plants here.",
+    "There are absolutely no exceptions to this rule.",
+    "That's a great achievement!",
+    "He brought her a bouquet of flowers.",
+    "Congratulations on your engagement!",
+    "Confetti rained down on the newlyweds.",
+    "Ottawa is the capital of Canada.",
+    "I ate a biscuit for snack.",
+    "I'm taking the case to the bureau.",
+    "The culprit had greasy hands.",
+    "There was intense competition at school.",
+    "I don't want to embarrass myself!",
+    "The description of the suspect matched him exactly.",
+    "The negotiator was able to engineer an escape plan."
+]
